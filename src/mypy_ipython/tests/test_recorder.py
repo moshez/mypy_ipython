251e1b571a7381e677b8b78d790468e60daee846
Moshe Zadka <zadka.moshe@gmail.com>
2020-07-19 13:57:33 -0700

    testing recorder

--- src/mypy_ipython/tests/test_recorder.py
+++ src/mypy_ipython/tests/test_recorder.py
@@ -10,3 +10,14 @@ def test_recorder_syntax_error():
     arecorder.process("x=1\n")
     arecorder.process("???\n")
     assert arecorder.relevant_snippets() == ["x=1\n"]
+
+def test_recorder_function():
+    arecorder = recorder.Recorder()
+    arecorder.process("def foo(): pass\n")
+    assert arecorder.relevant_snippets() == ["def foo(): pass\n"]
+
+def test_recorder_function_redefinition():
+    arecorder = recorder.Recorder()
+    arecorder.process("def foo(): pass\n")
+    arecorder.process("def foo(): print(5)\n")
+    assert arecorder.relevant_snippets() == ["def foo(): print(5)\n"]
