b1e97b9fae4654fd1d2d65904e036e7a50236418
Moshe Zadka <zadka.moshe@gmail.com>
2020-07-19 14:00:48 -0700

    post execute test

--- src/mypy_ipython/tests/test_recorder.py
+++ src/mypy_ipython/tests/test_recorder.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from mypy_ipython import recorder
 
 def test_recorder():
@@ -21,3 +23,10 @@ def test_recorder_function_redefinition():
     arecorder.process("def foo(): pass\n")
     arecorder.process("def foo(): print(5)\n")
     assert arecorder.relevant_snippets() == ["def foo(): print(5)\n"]
+
+def test_post_execute():
+    arecorder = recorder.Recorder()
+    shell = mock.MagicMock()
+    shell.history_manager.input_hist_parsed = ["x=1\n"]
+    recorder.post_execute(arecorder, shell)
+    assert arecorder.relevant_snippets() == ["x=1\n"]
