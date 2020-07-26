52a8e801513151b33e6ef148a47a7856941df55c
Moshe Zadka <zadka.moshe@gmail.com>
2020-07-25 21:38:38 -0700

    more black

--- setup.py
+++ setup.py
@@ -16,5 +16,9 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["attrs", "incremental", "mypy", "IPython"],
-    extras_require=dict(test=["pytest", "coverage"], lint=["black", "flake8", "mypy"], doc=["sphinx", "nbsphinx"]),
+    extras_require=dict(
+        test=["pytest", "coverage"],
+        lint=["black", "flake8", "mypy"],
+        doc=["sphinx", "nbsphinx"],
+    ),
 )
